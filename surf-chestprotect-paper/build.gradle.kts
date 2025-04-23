0cd3e5c95dd13ccde6146a23aeba531192714ce7
Jo_field <150937665+JoField08@users.noreply.github.com>
2025-04-24 00:34:48 +0200

    Implement database initialization in SurfChestProtectPaper on enable

--- surf-chestprotect-paper/build.gradle.kts
+++ surf-chestprotect-paper/build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
     id("dev.slne.surf.surfapi.gradle.paper-plugin")
 }
+
 surfPaperPluginApi {
     mainClass("dev.slne.surf.chestprotect.SurfChestProtectPaper")
     generateLibraryLoader(false)
     authors.add("Jo_field")
     authors.add("TheBjoRedCraft")
+}
 
+dependencies {
+    api(libs.surf.database)
 }
--- surf-chestprotect-paper/src/main/kotlin/dev/slne/surf/chestprotect/SurfChestProtectPaper.kt
+++ surf-chestprotect-paper/src/main/kotlin/dev/slne/surf/chestprotect/SurfChestProtectPaper.kt
@@ -1,10 +1,13 @@
 package dev.slne.surf.chestprotect
 
 import com.github.shynixn.mccoroutine.folia.SuspendingJavaPlugin
+import dev.slne.surf.database.DatabaseProvider
 import org.bukkit.plugin.java.JavaPlugin
 
 class SurfChestProtectPaper(): SuspendingJavaPlugin() {
+    val plugin = JavaPlugin.getPlugin(SurfChestProtectPaper::class.java)
 
+    override suspend fun onEnableAsync() {
+        DatabaseProvider(plugin.dataFolder.toPath(), plugin.dataFolder.toPath())
+    }
 }
-
-val plugin = JavaPlugin.getPlugin(SurfChestProtectPaper::class.java)
